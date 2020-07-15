f93ef2c13a7bae5a37a1f0a8de39c6023d2ba930
Vaishnav Murali <vaishnavmurali@gmail.com>
2020-07-15 13:54:54 +0530

    Detailed CSV
    Signed-off-by: Vaishnav Murali <vaishnavmurali@gmail.com>

--- pystdf/Writers.py
+++ pystdf/Writers.py
@@ -152,3 +152,138 @@ class CsvWriter:
     def after_complete(self, dataSource):
         # self.stream.write('</Stdf>\n')
         self.stream.flush()
+
+class FileDetailCsvWriter:
+    extra_entities = {'\0': ''}
+
+    @staticmethod
+    def csv_format(rectype, field_index, value):
+        field_type = rectype.fieldStdfTypes[field_index]
+        if value is None:
+            return ""
+        elif rectype is V4.gdr:
+            return ';'.join([str(v) for v in value])
+        elif field_type[0] == 'k': # An Array of some other type
+            return ','.join([format_by_type(v, field_type[2:]) for v in value])
+        elif rectype is V4.mir or rectype is V4.mrr:
+            field_name = rectype.fieldNames[field_index]
+            if field_name.endswith('_T'): # A Date-Time in an MIR/MRR
+                return strftime('%H:%M:%ST%d-%b-%Y', localtime(value))
+            else:
+                return str(value)
+        else:
+            return str(value)
+
+    def __init__(self, stream=sys.stdout):
+        self.stream = stream
+        self.CURR_SQ = None
+        self.start_timestamp = None
+        self.end_timestamp = None
+        self.file_name = None
+        self.temperature = None
+        self.part_id_dict = {}
+        self.part_count = 1
+
+    def before_begin(self, dataSource):
+        self.stream.write('file_name,start_timestamp,end_timestamp,temperature_ran\n')
+
+    def after_send(self, dataSource, data):
+        if data[0].__class__.__name__.lower() == 'mir':
+            self.file_name = self.csv_format(data[0],data[0].fieldNames.index("USER_TXT"), data[1][data[0].fieldNames.index("USER_TXT")])
+            try:
+                self.temperature = self.file_name.split("_")[2]
+            except Exception as E:
+                print("No temperature found")
+            self.start_timestamp = data[1][data[0].fieldNames.index("START_T")]
+
+        elif data[0].__class__.__name__.lower() == 'mrr':
+            self.end_timestamp = data[1][data[0].fieldNames.index("FINISH_T")]
+            self.stream.write(f'{self.file_name},{self.start_timestamp},{self.end_timestamp},{self.temperature}')
+        # self.stream.write('/>\n')
+
+    def after_complete(self, dataSource):
+        self.stream.flush()
+
+class DetailedCsvWriter:
+    extra_entities = {'\0': ''}
+
+    @staticmethod
+    def csv_format(rectype, field_index, value):
+        field_type = rectype.fieldStdfTypes[field_index]
+        if value is None:
+            return ""
+        elif rectype is V4.gdr:
+            return ';'.join([str(v) for v in value])
+        elif field_type[0] == 'k': # An Array of some other type
+            return ','.join([format_by_type(v, field_type[2:]) for v in value])
+        elif rectype is V4.mir or rectype is V4.mrr:
+            field_name = rectype.fieldNames[field_index]
+            if field_name.endswith('_T'): # A Date-Time in an MIR/MRR
+                return strftime('%H:%M:%ST%d-%b-%Y', localtime(value))
+            else:
+                return str(value)
+        else:
+            return str(value)
+
+    def __init__(self, stream=sys.stdout,stream2=sys.stdout):
+        self.stream2 = stream2
+        self.stream = stream
+        self.CURR_SQ = None
+        self.start_timestamp = None
+        self.end_timestamp = None
+        self.file_name = None
+        self.temperature = None
+        self.part_id_dict = {}
+        self.part_count = 1
+
+    def before_begin(self, dataSource):
+        self.stream2.write('file_name,start_timestamp,end_timestamp,temperature_ran\n')
+        self.stream.write('file_name,sequence_no,part_no,test_number,head_num,site_number,test_text,result,pass_or_fail\n')
+
+    def after_send(self, dataSource, data):
+        # self.stream.write('<%s' % (data[0].__class__.__name__))
+        # for i, val in enumerate(data[1]):
+        
+        if data[0].__class__.__name__.lower() == 'bps' and 'SEQ_NAME' in data[0].fieldNames:
+            fmtval = self.csv_format(data[0],data[0].fieldNames.index("SEQ_NAME"), data[1][data[0].fieldNames.index("SEQ_NAME")])
+            self.CURR_SQ = quoteattr(fmtval, self.extra_entities)
+        
+        # elif data[0].__class__.__name__.lower() == 'eps':
+        #     self.part_id_dict = {}
+        
+        elif data[0].__class__.__name__.lower() == 'pir':
+            siteNum = self.csv_format(data[0],data[0].fieldNames.index("SITE_NUM"), data[1][data[0].fieldNames.index("SITE_NUM")])
+            headNum = self.csv_format(data[0],data[0].fieldNames.index("HEAD_NUM"), data[1][data[0].fieldNames.index("HEAD_NUM")])
+            self.part_id_dict[f"{headNum}-{siteNum}"] = self.part_count
+            self.part_count+=1
+        
+        elif data[0].__class__.__name__.lower() == 'ptr':
+            result = self.csv_format(data[0],data[0].fieldNames.index("RESULT"), data[1][data[0].fieldNames.index("RESULT")])
+            testText = self.csv_format(data[0],data[0].fieldNames.index("TEST_TXT"), data[1][data[0].fieldNames.index("TEST_TXT")])
+            testNum = self.csv_format(data[0],data[0].fieldNames.index("TEST_NUM"), data[1][data[0].fieldNames.index("TEST_NUM")])
+            siteNum = self.csv_format(data[0],data[0].fieldNames.index("SITE_NUM"), data[1][data[0].fieldNames.index("SITE_NUM")])
+            headNum = self.csv_format(data[0],data[0].fieldNames.index("HEAD_NUM"), data[1][data[0].fieldNames.index("HEAD_NUM")])
+            testFlag = self.csv_format(data[0],data[0].fieldNames.index("TEST_FLG"), data[1][data[0].fieldNames.index("TEST_FLG")])
+            if int(testFlag) > 0:
+                testFlag = 0
+            else:
+                testFlag = 1
+            headID = f'{headNum}-{siteNum}'
+            self.stream.write(f'{self.file_name},{self.CURR_SQ},{self.part_id_dict[headID]},{testNum},{headNum},{siteNum},"{testText}",{result},{testFlag}\n')
+        
+        elif data[0].__class__.__name__.lower() == 'mir':
+            self.file_name = self.csv_format(data[0],data[0].fieldNames.index("USER_TXT"), data[1][data[0].fieldNames.index("USER_TXT")])
+            try:
+                self.temperature = self.file_name.split("_")[2]
+            except Exception as E:
+                print("No temperature found")
+            self.start_timestamp = data[1][data[0].fieldNames.index("START_T")]
+
+        elif data[0].__class__.__name__.lower() == 'mrr':
+            self.end_timestamp = data[1][data[0].fieldNames.index("FINISH_T")]
+            self.stream2.write(f'{self.file_name},{self.start_timestamp},{self.end_timestamp},{self.temperature}')
+        # self.stream.write('/>\n')
+
+    def after_complete(self, dataSource):
+        # self.stream.write('</Stdf>\n')
+        self.stream.flush()
